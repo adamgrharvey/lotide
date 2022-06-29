b58e2657f148927168c867a8a5abbd6a46c5be25
Adam <adamgrharvey@gmail.com>
2022-06-28 17:38:10 -0700

    function that posts to console if two arrays are equal or not

---
+++ reverse.js
@@ -0,0 +1,12 @@
+const reverse = function() {
+  let out = "";
+  for (let i = 2; i < process.argv.length; i++) {
+    for (let j = process.argv[i].length - 1; j >= 0; j--) {
+      out += process.argv[i][j];
+    }
+    console.log(out);
+    out = "";
+  }
+};
+
+reverse();
