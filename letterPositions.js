6cd8869e65d6cd87b598236ec264bcb4478f55d3
Adam <adamgrharvey@gmail.com>
2022-06-29 13:54:00 -0700

    outputs an object that shows index locations of each letter in a given string

---
+++ letterPositions.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(string) {
+  const letterPositions = { };
+  for (const i in string) {
+    if (letterPositions[string[i]]) {
+      letterPositions[string[i]].push(i);
+    } else {
+      letterPositions[string[i]] = [i];
+    }
+  }
+  return letterPositions;
+};
+
+console.log(letterPositions('Hey harry'));
