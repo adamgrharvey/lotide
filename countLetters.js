0dcf4ea6cccd98695b3ed58c384ba0ebfed7b266
Adam <adamgrharvey@gmail.com>
2022-06-29 13:48:42 -0700

    function that counts unique letters in a string, and returns the result as an object

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+const uniqueLetters = { };
+for (const i in string) {
+  if (uniqueLetters[string[i]]) {
+    uniqueLetters[string[i]]++;
+  }
+  else {
+    uniqueLetters[string[i]] = 1;
+  }
+}
+return uniqueLetters;
+};
+
+console.log(countLetters('LHL'));
